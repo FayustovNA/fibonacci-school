f2d599bb2ab15b61f7ca2efe20e3e74fc01fa7bd
FayustovNA <n.a.fayustov@gmail.com>
2023-05-07 15:25:01 +0300

    last 07052023

---
+++ src/components/list-page/list-page-functions.ts
@@ -0,0 +1,225 @@
+
+interface IStack<T> {
+    pushTail: (item: T) => void;
+    pushHead: (item: T) => void;
+    pushByIndex: (index: number, value: T) => void;
+    deleteHead: () => void;
+    deleteTail: () => void;
+    deleteByIndex: (index: number) => void;
+}
+
+export class Node<T> {
+    value: T;
+    next: Node<T> | null;
+    constructor(value: T, next?: Node<T> | null) {
+        this.value = value;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+export class LinkedList<T> implements IStack<T> {
+    private top: Node<T> | null = null;
+    private tail: Node<T> | null = null;
+    private size: number;
+
+
+    constructor(startList: any) {
+        this.top = null;
+        this.tail = null;
+        this.size = 0;
+        for (let item of startList) {
+            this.pushHead(item);
+            console.log(this.size)
+        }
+    }
+
+    getHead() {
+        if (this.top === null) {
+            return null;
+        }
+        return this.top;
+    }
+
+    getTail() {
+        if (this.tail === null) {
+            return null;
+        }
+        let tail: any = this.top;
+        while (tail.next !== null) {
+            tail = tail.next;
+        }
+        return tail;
+    }
+
+    pushHead = (item: T): any => {
+        const newNode = new Node(item, this.top);
+        this.top = newNode;
+        if (!this.tail) {
+            this.tail = newNode;
+        }
+        this.size++;
+        return this;
+    };
+
+    pushTail = (item: T): any => {
+        const newNode = new Node(item);
+
+        if (!this.top || !this.tail) {
+            this.top = newNode;
+            this.tail = newNode;
+            this.size++;
+            return this
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+            this.size++;
+            return this
+        };
+    };
+
+    deleteHead = (): any => {
+        if (this.isEmpty()) {
+            throw new Error("stack is empty");
+        }
+        if (!this.top) {
+            return null;
+        }
+        this.top = this.top.next;
+        this.size--;
+    };
+
+    deleteTail = (): any => {
+        if (this.isEmpty()) {
+            throw new Error("stack is empty");
+        }
+        if (!this.tail) {
+            return null;
+        }
+        if (this.size === 1) {
+            this.top = null;
+            this.tail = null;
+            this.size--;
+            return this
+        }
+        let currentItem = this.top;
+        let previousItem: any = null;
+
+        while (currentItem?.next) {
+            previousItem = currentItem;
+            currentItem = currentItem.next
+        }
+
+        previousItem.next = null;
+        this.tail = previousItem;
+        this.size--;
+        console.log(this.size)
+        return this
+    };
+
+    pushByIndex(index: number, value: T) {
+        if (index < 0 || index > this.size) {
+            //
+            console.log('Enter a valid index');
+            return;
+        } else {
+            const node = new Node(value);
+            if (index === 0) {
+                node.next = this.top;
+                this.top = node;
+                console.log(node)
+            } else {
+                let curr = this.top;
+                let currIndex = 0;
+                while (curr?.next && currIndex < index) {
+                    currIndex++
+                    if (currIndex !== index) {
+                        curr = curr.next
+                    }
+                }
+                if (curr) {
+                    node.next = curr.next
+                    curr.next = node
+                }
+            }
+            this.size++;
+        }
+    }
+
+    deleteByIndex(index: number) {
+        if (index < 0 || index >= this.size) {
+            console.log('Enter a valid index');
+        }
+
+        if (index === 0) {
+            return this.deleteHead();
+        }
+        let currentItem: any = this.top;
+        let previousItem: any = null;
+        for (let i = 0; i < this.size; i++) {
+            if (i !== index) {
+                previousItem = currentItem;
+                currentItem = currentItem.next;
+            } else {
+                previousItem.next = currentItem.next;
+                this.size--;
+                return this
+            }
+        }
+        return false
+    }
+
+    findElement(item: any) {
+        if (!this.top) {
+            return null;
+        }
+        let currentNode: any = this.top;
+        while (currentNode) {
+            if (currentNode.value === item) {
+                return currentNode
+            }
+            currentNode = currentNode.next;
+        }
+        return null
+    }
+
+    toArray() {
+        const listArray = [];
+        let currentNode = this.top;
+
+        while (currentNode) {
+            listArray.push(currentNode);
+            currentNode = currentNode.next;
+        }
+        return listArray
+    }
+
+
+    isEmpty = (): boolean => {
+        return this.top === null;
+    };
+}
+
+//Получение рандомного массива
+function getRandomArbitrary(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+}
+
+export function randomArr(): any {
+    const minValue = 0;
+    const maxValue = 100;
+    const minLength = 1;
+    const maxLength = 5;
+    const arrLength = Math.round(getRandomArbitrary(minLength, maxLength));
+
+    const arr = [];
+
+    for (let i = 0; i < arrLength; i++) {
+        const randomItem = Math.round(getRandomArbitrary(minValue, maxValue));
+        arr.push(String(randomItem));
+    }
+
+    return arr;
+}
+
+
+export const linkedList = new LinkedList<any>(randomArr());
--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,16 +1,105 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { ArrowIcon } from "../ui/icons/arrow-icon";
 import { useState, ChangeEvent, useEffect } from "react";
 import { ElementStates } from "../../types/element-states";
 import style from "./list-page.module.css";
 import { Input } from "../ui/input/input";
 import { Circle } from "../ui/circle/circle";
 import { Button } from "../ui/button/button";
+import { linkedList, Node } from "./list-page-functions";
 
 
 export const ListPage: React.FC = () => {
 
+  const [list, setList] = useState<Node<string>[]>(linkedList.toArray());
+  console.log(list)
+  const [inputValue, setInputValue] = useState({
+    value: '',
+    index: '',
+  });
 
+  const [loader, setLoader] = useState({
+    pushTail: false,
+    pushHead: false,
+    deleteTail: false,
+    deleteHead: false,
+    pushIndex: false,
+    deleteIndex: false,
+  });
+
+  // const [isActive, setIsActive] = useState<any>(null);
+
+  //Время анимации
+  function setTime() {
+    return new Promise<void>((res) => setTimeout(res, 500));
+  }
+
+  //Сбор данных из формы
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement
+    setInputValue({ ...inputValue, [target.name]: target.value });
+  };
+
+  //Добавить в начало
+  const addHead = async () => {
+    setLoader({ ...loader, pushHead: true })
+    await setTime();
+    linkedList.pushHead(inputValue.value)
+    await setTime();
+    setList([...linkedList.toArray()])
+    console.log(list)
+    setInputValue({ value: "", index: "" });
+    setLoader({ ...loader, pushHead: false });
+  }
+
+  //Добавить в конец
+  const addTail = async () => {
+    setLoader({ ...loader, pushTail: true });
+    await setTime();
+    linkedList.pushTail(inputValue.value)
+    setList([...linkedList.toArray()])
+    setInputValue({ value: "", index: "" });
+    setLoader({ ...loader, pushTail: false });
+  }
+
+  //Удалить из начала
+  const deleteHead = async () => {
+    setLoader({ ...loader, deleteHead: true });
+    await setTime();
+    linkedList.deleteHead()
+    setList([...linkedList.toArray()])
+    setLoader({ ...loader, deleteHead: false });
+  }
+
+  //Удалить из конца
+  const deleteTail = async () => {
+    setLoader({ ...loader, deleteTail: true });
+    await setTime();
+    linkedList.deleteTail()
+    setList([...linkedList.toArray()])
+    setLoader({ ...loader, deleteTail: false });
+  }
+
+  //Добавить по индексу
+  const addByIndex = async () => {
+    setLoader({ ...loader, pushIndex: true });
+    await setTime();
+    linkedList.pushByIndex(Number(inputValue.index), inputValue.value)
+    setList([...linkedList.toArray()])
+    setInputValue({ value: "", index: "" });
+    setLoader({ ...loader, pushIndex: false });
+  }
+
+  //Удалить по индексу
+  const deleteByIndex = async () => {
+    setLoader({ ...loader, deleteIndex: true });
+    await setTime();
+    linkedList.deleteByIndex(Number(inputValue.index))
+    setList([...linkedList.toArray()])
+    setInputValue({ value: "", index: "" });
+    setLoader({ ...loader, deleteIndex: false });
+  }
 
   return (
     <SolutionLayout title="Связный список">
@@ -19,81 +108,85 @@ export const ListPage: React.FC = () => {
           <Input
             maxLength={4}
             isLimitText
-            // value={inputValue}
-            // onChange={onChange}
+            name='value'
+            value={inputValue.value}
+            onChange={onChange}
             placeholder="Введите значение"
           />
           <Button
             text={'Добавить в head'}
             type={'button'}
             extraClass={style.button}
-          // isLoader={loader.enqueue}
-          // onClick={enQueue}
-          // disabled={!inputValue}
+            isLoader={loader.pushHead}
+            onClick={addHead}
+            disabled={!inputValue.value}
           />
           <Button
             text={'Добавить в tail'}
             type={'button'}
             extraClass={style.button}
-          // isLoader={loader.enqueue}
-          // onClick={enQueue}
-          // disabled={!inputValue}
+            isLoader={loader.pushTail}
+            onClick={addTail}
+            disabled={!inputValue.value}
           />
           <Button
             text={'Удалить из head'}
             type={'button'}
             extraClass={style.button}
-          // isLoader={loader.dequeue}
-          // onClick={deQueue}
-          // disabled={queue.isEmpty()}
+            isLoader={loader.deleteHead}
+            onClick={deleteHead}
+            disabled={linkedList.isEmpty()}
           />
           <Button
             text={'Удалить из tail'}
             type={'button'}
             extraClass={style.button}
-          // isLoader={loader.dequeue}
-          // onClick={deQueue}
-          // disabled={queue.isEmpty()}
+            isLoader={loader.deleteTail}
+            onClick={deleteTail}
+            disabled={linkedList.isEmpty()}
           />
         </div>
 
         <div className={style.forminputindex}>
           <Input
             type="number"
-            // value={inputValue}
-            // onChange={onChange}
+            name='index'
+            value={inputValue.index}
+            onChange={onChange}
             placeholder="Введите индекс"
           />
           <Button
             text={'Добавить по индексу'}
             type={'button'}
             extraClass={style.button_max}
-          // isLoader={loader.reset}
-          // onClick={resetQueue}
-          // disabled={queue.isEmpty()}
+            isLoader={loader.pushIndex}
+            onClick={addByIndex}
+            disabled={!inputValue.index || !inputValue.value}
           />
           <Button
             text={'Удалить по индексу'}
             type={'button'}
             extraClass={style.button_max}
-          // isLoader={loader.reset}
-          // onClick={resetQueue}
-          // disabled={queue.isEmpty()}
+            isLoader={loader.deleteIndex}
+            onClick={deleteByIndex}
+            disabled={linkedList.isEmpty() || !inputValue.index}
           />
         </div>
       </div>
-      {/* <ul className={style.line}>
-        {queues?.map((item: any, index: number) =>
-          <Circle
-            key={index}
-            letter={item}
-            index={index}
-            head={index === queue.getHead() && !queue.isEmpty() ? 'head' : ""}
-            tail={index + 1 === queue.getTail() && !queue.isEmpty() ? 'tail' : ""}
-            state={index === isActive ? ElementStates.Changing : ElementStates.Default}
-          />
+      <ul className={style.line}>
+        {list?.map((item: any, index: number) =>
+          <div className={style.lineitem} key={index}>
+            <Circle
+              key={index}
+              letter={item.value}
+              index={index}
+              extraClass={style.circle}
+            />
+            {index !== list.length - 1 && <ArrowIcon />}
+          </div>
+
         )}
-      </ul> */}
+      </ul>
     </SolutionLayout>
   );
 };
