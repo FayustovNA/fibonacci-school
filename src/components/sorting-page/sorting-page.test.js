f8d3583332d12ee4e3abee280be935ed38cf3248
Nikolay Fayustov <n.a.fayustov@gmail.com>
2023-05-27 14:42:21 +0300

    first part done

--- src/components/sorting-page/sorting-page.test.js
+++ src/components/sorting-page/sorting-page.test.js
@@ -1,4 +1,75 @@
-// Корректно сортирует: 
+import { bubbleSortDownTest, bubbleSortUpTest, selectionSortDownTest, selectionSortUpTest } from "./sorting-page-functions";
+
+// Корректно сортирует 
 // пустой массив;
 // массив из одного элемента;
 // массив из нескольких элементов
+
+describe('selectionSortUpTest', () => {
+
+    it('should correctly sort an empty array', async () => {
+        expect(await selectionSortUpTest([])).toEqual([])
+    });
+
+    it('should correctly sort an array with one element', async () => {
+        expect(await selectionSortUpTest([1])).toEqual([1])
+    });
+
+    it('should correctly sort an array with multiple elements', async () => {
+        expect(await selectionSortUpTest([5, 4, 3, 2, 1])).toEqual([1,2,3,4,5])
+    });
+
+  });
+
+
+  describe('selectionSortDownTest', () => {
+
+    it('should correctly sort an empty array', async () => {
+        expect(await selectionSortDownTest([])).toEqual([])
+    });
+
+    it('should correctly sort an array with one element', async () => {
+        expect(await selectionSortDownTest([1])).toEqual([1])
+    });
+
+    it('should correctly sort an array with multiple elements', async () => {
+        expect(await selectionSortDownTest([1, 2, 3, 4, 5])).toEqual([5,4,3,2,1])
+    });
+
+  });
+
+
+
+  
+  describe('bubbleSortUpTest', () => {
+
+    it('should correctly sort an empty array', async () => {
+        expect(await bubbleSortUpTest([])).toEqual([])
+    });
+
+    it('should correctly sort an array with one element', async () => {
+        expect(await bubbleSortUpTest([1])).toEqual([1])
+    });
+
+    it('should correctly sort an array with multiple elements', async () => {
+        expect(await bubbleSortUpTest([5, 4, 3, 2, 1])).toEqual([1,2,3,4,5])
+    });
+
+  });
+
+  describe('bubbleSortDownTest', () => {
+
+    it('should correctly sort an empty array', async () => {
+        expect(await bubbleSortDownTest([])).toEqual([])
+    });
+
+    it('should correctly sort an array with one element', async () => {
+        expect(await bubbleSortDownTest([1])).toEqual([1])
+    });
+
+    it('should correctly sort an array with multiple elements', async () => {
+        expect(await bubbleSortDownTest([1, 2, 3, 4, 5])).toEqual([5,4,3,2,1])
+    });
+
+  });
+
--- src/components/string/string.test.js
+++ src/components/string/string.test.js
@@ -1,5 +1,38 @@
+import { reverseString } from "./utils";
 // Корректно разворачивает строку
 // с чётным количеством символов.
 // с нечетным количеством символов.
 // с одним символом.
 // пустую строку
+
+describe("reverseString function", () => {
+    it("should reverse a string with even number of characters", async () => {
+      expect(await reverseString("test")).toEqual([
+        { item: "t", state: 'modified' },
+        { item: "s", state: 'modified' },
+        { item: "e", state: 'modified'},
+        { item: "t", state: 'modified'}
+    ]);
+});
+
+it('should reverse a string with odd number of characters', async () => {
+    expect(await reverseString("world")).toEqual([
+      { item: "d", state: 'modified' },
+      { item: "l", state: 'modified' },
+      { item: "r", state: 'modified'},
+      { item: "o", state: 'modified'},
+      { item: "w", state: 'modified'}
+  ]);
+});
+
+it('should return an array with one element if given a string with one character', async () => {
+    expect(await reverseString("a")).toEqual([
+      { item: "a", state: 'modified' },
+  ]);
+});
+
+it('should return an empty array if given an empty string', async () => {
+    expect(await reverseString("")).toEqual([]);
+});
+
+});
